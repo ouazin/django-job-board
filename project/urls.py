5a89c448df4e3a57acdc739c58f453ee4bc2b7bf
OUAZIN Nabil <nabilouazin91@gmail.com>
2022-09-02 22:04:39 +0100

    some kinds of

--- project/urls.py
+++ project/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    path('', include('home.urls')),
-    path('', include('accounts.urls')),
-    path('', include('contact.urls')),
+    #path('', include('home.urls')),
+    #path('', include('accounts.urls')),
+    #path('', include('contact.urls')),
 ]
